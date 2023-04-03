53aa9fa816b2202824b80627cf90073f6e6ba5cd
a60231023 <ayushtiwari6061@gmail.com>
2023-04-03 18:57:47 +0530

    get single user done 100%

--- test/getUser.js
+++ test/getUser.js
@@ -1,22 +1,19 @@
+const { before } = require("mocha");
 const chai = require("chai");
 const expect = chai.expect;
 const should = chai.should();
 const url = require("./testUtils");
 const axios = require("axios");
 
-const createUser = async () => {
-  const payload = {
-    name: "new user",
-    email: "new@gmail.com",
-    age: 50,
-  };
-  const response = await axios.post(url.createUser, payload);
-//   console.log(response.data);
-  return response;
-};
+
+before(async() => {
+  newUserResponse = await url.createUserFunc();
+});
+
+
+
 
 describe("Get user info", function () {
-  
   it("Get all user", async () => {
     const response = await axios.get(url.getAllUser);
     const data = await response.data;
@@ -24,14 +21,22 @@ describe("Get user info", function () {
     expect(response.data.user).to.be.an("array");
   });
 
-  it("Get single user", async () => {
-    const newUserResponse = await createUser();
-    console.log(newUserResponse.data);
+  it("Get single user", async () => {    
+    // console.log(newUserResponse.data);
     const newUser = newUserResponse.data.user;
-    const response = await axios.get(
-      url.singleUser + "/" + newUser._id
-    );
+    const response = await axios.get(url.singleUser + "/" + newUser._id);
     console.log(response.data);
+    expect(response.status).to.be.equal(200);
+    expect(response.data.singleUser).to.be.an("object");
+  });
+
+  it("Should have all the property for each user", async() => {
+    const newUser = newUserResponse.data.user;
+    const response = await axios.get(url.singleUser + "/" + newUser._id);
+    // console.log(response.data);
+    response.data.singleUser.should.have.property("name");
+    response.data.singleUser.should.have.property("email");
+    response.data.singleUser.should.have.property("age");
     expect(response.status).to.be.equal(200);
     expect(response.data.singleUser).to.be.an("object");
   });
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,11 +1,25 @@
-const API_URL = "http://localhost:8000/";
+const axios = require("axios");
 
+const API_URL = "http://localhost:8000/";
 const getAllUser = API_URL + "getAllUser";
 const singleUser = API_URL + "getSingleUser";
 const createUser = API_URL + "register";
+
+
+const createUserFunc = async () => {
+  const payload = {
+    name: "new user",
+    email: "new@gmail.com",
+    age: 50,
+  };
+  const response = await axios.post(createUser, payload);
+//   console.log(response.data);
+  return response;
+};
 
 module.exports = {
   getAllUser,
   singleUser,
   createUser,
+  createUserFunc,
 };
