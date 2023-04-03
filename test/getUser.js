33bcdb6d5aa121fc27c1979a46325feddcb38e53
a60231023 <ayushtiwari6061@gmail.com>
2023-04-03 14:33:39 +0530

    get all user endpoint for testing done

--- test/getUser.js
+++ test/getUser.js
@@ -1,32 +1,57 @@
 const chai = require('chai');
 const expect = chai.expect;
 const should = chai.should();
+const url = require('./testUtils');
+const axios = require('axios');
+const { response } = require('express');
 
 
-describe("check two number", function(){
+describe("Get user info", function(){
 
-    it("match two number", () => {
-        
-        let first = 10;
-        let second = 10;
+    it("Get all user", async() => {
+        const response = await axios.get(url.getalluser);
+        const data = await response.data
+        console.log(data)
+        expect(response.status).to.be.equal(200);
+        expect(response.data.user).to.be.an("array")
 
-        expect(first).to.be.equal(second); 
+    });
+    // id is already there in the url but you can take here also 
+    it("Get single user", async() => {
+        const  response = await axios.get(url.singleuser);
+        const data = await response.data;
+
+
     });
 
-    it("should not match two number", () => {
+
+});
+
+
+// describe("check two number", function(){
+
+//     it("match two number", () => {
         
-        let first = 100;
-        let second = 10;
+//         let first = 10;
+//         let second = 10;
 
-        expect(first).not.to.be.equal(second); 
-    });
+//         expect(first).to.be.equal(second); 
+//     });
 
-    it("should have matching name", () => {
-        let first = "ayush";
-        let second = "ayush";
+//     it("should not match two number", () => {
+        
+//         let first = 100;
+//         let second = 10;
 
-        expect(first).to.be.equal(second);
-    })
+//         expect(first).not.to.be.equal(second); 
+//     });
+
+//     it("should have matching name", () => {
+//         let first = "ayush";
+//         let second = "ayush";
+
+//         expect(first).to.be.equal(second);
+//     })
 
      
-})
+// })
---
+++ test/testUtils.js
@@ -0,0 +1,9 @@
+const API_URL = "http://localhost:8000/";
+
+const getalluser = API_URL + "getAllUser";
+const singleuser = API_URL + "getSingleUser/642923ad5203d23c9e13ec43"
+
+module.exports = {
+    getalluser,
+    singleuser,
+};
